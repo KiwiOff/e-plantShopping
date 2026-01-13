26984e35cd9e136eaf619f76a934696536dda129
KiwiOff <matiasquihui2@gmail.com>
2026-01-13 01:26:20 -0500

    Un avance

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
@@ -272,9 +272,31 @@ function ProductList({ onHomeClick }) {
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
+            
             {!showCart ? (
                 <div className="product-grid">
+                    
+                    {plantsArray.map( (category, index) => {
+                        return (
+                            <div key={index} className="plantname_heading">
+                                <h2 className='plant_heading'>{category.category}</h2>
+                                <div className="product-list">
+                                    {category.plants.map((nameplant,idx) => {
+                                        return(
+                                            <div key={idx} className='product-card'>
+                                                <h3>{nameplant.name}</h3>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
 
+
+                            </div>
+                            
+                            
+                        )
+                        
+                    } )}
 
                 </div>
             ) : (
